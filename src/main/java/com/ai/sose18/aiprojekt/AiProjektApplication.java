d49ce7a79554c9302adbb38e6eb1fdb9fc11fc94
Tu Nguyen Anh <itoo.anders@gmx.de>
2018-04-30 17:58:11 +0200

    noch einmal

---
+++ src/main/java/com/ai/sose18/aiprojekt/AiProjektApplication.java
@@ -0,0 +1,12 @@
+package com.ai.sose18.aiprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProjektApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ai/sose18/aiprojekt/AiProjektApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ai.sose18.aiprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AiProjektApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
